c09b5b69ba17589c17e54fb36371c2070bbc43bc
Mansoor Ahmad <iammansoor9906@gmail.com>
2026-01-05 23:08:15 +0500

    Update api

--- src/modules/home/api/coins/coins.client.ts
+++ src/modules/home/api/coins/coins.client.ts
@@ -2,5 +2,5 @@ import createClient from 'openapi-fetch';
 import type { paths } from '../../../../api/services/coins/coins';
 
 export const coinsApiClient = createClient<paths>({
-  baseUrl: import.meta.env.VITE_API_BASE_URL ?? 'https://localhost:7151',
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
 });
--- src/modules/home/api/market/market.client.ts
+++ src/modules/home/api/market/market.client.ts
@@ -2,5 +2,5 @@ import createClient from 'openapi-fetch';
 import type { paths } from '../../../../api/services/market/market';
 
 export const marketApiClient = createClient<paths>({
-  baseUrl: import.meta.env.VITE_API_BASE_URL ?? 'https://localhost:7151',
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
 });
