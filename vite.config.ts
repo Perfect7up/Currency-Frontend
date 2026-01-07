6ed0b0d38e392f4e48edeede27bce80809cdd292
Mansoor Ahmad <iammansoor9906@gmail.com>
2026-01-07 18:18:18 +0500

    feat: add commitlint and husky hooks

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
     tailwindcss(),
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['test/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
 });
